1b214b5c881d9197e9dddb3262bb3b7fb60b589a
pizzoli <matia.pizzoli@gmail.com>
2015-09-17 23:42:04 +0200

    moved nested structure for denoiser device data

--- include/rmd/depthmap_denoiser.cuh
+++ include/rmd/depthmap_denoiser.cuh
@@ -6,6 +6,36 @@
 namespace rmd
 {
 
+namespace denoise
+{
+
+struct DeviceData
+{
+  DeviceData(
+      DeviceImage<float> * const u_dev_ptr,
+      DeviceImage<float> * const u_head_dev_ptr,
+      DeviceImage<float> * const p_dev_ptr,
+      DeviceImage<float> * const g_dev_ptr,
+      const size_t &w,
+      const size_t &h);
+  const float L;
+  const float tau;
+  const float sigma;
+  const float theta;
+
+  DeviceImage<float> * const u;
+  DeviceImage<float> * const u_head;
+  DeviceImage<float> * const p;
+  DeviceImage<float> * const g;
+
+  const size_t width;
+  const size_t height;
+
+  float large_sigma_sq;
+};
+
+} // denoise namespace
+
 class DepthmapDenoiser
 {
 
@@ -25,33 +55,13 @@ private:
   DeviceImage<float> p_;
   DeviceImage<float> g_;
 
-  struct DeviceData
-  {
-    DeviceData(
-        DeviceImage<float> * const u_dev_ptr,
-        DeviceImage<float> * const u_head_dev_ptr,
-        DeviceImage<float> * const p_dev_ptr,
-        DeviceImage<float> * const g_dev_ptr);
-    const float L;
-    const float tau;
-    const float sigma;
-    const float theta;
-
-    DeviceImage<float> * const u;
-    DeviceImage<float> * const u_head;
-    DeviceImage<float> * const p;
-    DeviceImage<float> * const g;
-
-    float large_sigma_sq;
-  };
-
-  DeviceData * host_ptr;
-  DeviceData * dev_ptr;
+  denoise::DeviceData * host_ptr;
+  denoise::DeviceData * dev_ptr;
 
   dim3 dim_block_;
   dim3 dim_grid_;
 };
 
-}
+} // rmd namespace
 
 #endif // RMD_DEPTHMAP_DENOISER_CUH
