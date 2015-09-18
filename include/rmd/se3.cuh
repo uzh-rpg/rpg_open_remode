81d4809c9e77d095d44f744a7c1c3f68680997fc
pizzoli <matia.pizzoli@gmail.com>
2015-09-18 23:44:59 +0200

    added license header

--- include/rmd/se3.cuh
+++ include/rmd/se3.cuh
@@ -1,9 +1,19 @@
-/*
- * se3.cuh
- *
- *  Created on: Feb 10, 2014
- *      Author: Matia Pizzoli
- */
+// This file is part of REMODE - REgularized MOnocular Depth Estimation.
+//
+// Copyright (C) 2014 Matia Pizzoli <matia dot pizzoli at gmail dot com>
+// Robotics and Perception Group, University of Zurich, Switzerland
+// http://rpg.ifi.uzh.ch
+//
+// REMODE is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or any later version.
+//
+// REMODE is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef RMD_SE3_CUH_
 #define RMD_SE3_CUH_
--- test/dataset.cpp
+++ test/dataset.cpp
@@ -1,3 +1,20 @@
+// This file is part of REMODE - REgularized MOnocular Depth Estimation.
+//
+// Copyright (C) 2014 Matia Pizzoli <matia dot pizzoli at gmail dot com>
+// Robotics and Perception Group, University of Zurich, Switzerland
+// http://rpg.ifi.uzh.ch
+//
+// REMODE is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or any later version.
+//
+// REMODE is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "dataset.h"
 
 const std::string & rmd::test::DatasetEntry::getImageFileName() const
