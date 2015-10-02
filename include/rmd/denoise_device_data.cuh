b842e1c0df0f3788cf91e72631690057311820ed
pizzoli <matia.pizzoli@gmail.com>
2015-10-03 00:49:55 +0200

    namespaced DeviceData for the denoiser

--- include/rmd/denoise_device_data.cuh
+++ include/rmd/denoise_device_data.cuh
@@ -15,41 +15,45 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef RMD_DEPTHMAP_DENOISER_CUH
-#define RMD_DEPTHMAP_DENOISER_CUH
+#ifndef RMD_DENOISE_DEVICE_DATA_CUH
+#define RMD_DENOISE_DEVICE_DATA_CUH
 
 #include <rmd/device_image.cuh>
-#include <rmd/denoise_device_data.cuh>
 
 namespace rmd
 {
 
-class DepthmapDenoiser
+namespace denoise
 {
-public:
-  DepthmapDenoiser(size_t width, size_t height);
-  ~DepthmapDenoiser();
-  void denoise(const rmd::DeviceImage<float> &mu,
-               const rmd::DeviceImage<float> &sigma_sq,
-               const rmd::DeviceImage<float> &a,
-               const rmd::DeviceImage<float> &b,
-               float *host_denoised,
-               float lambda,
-               int iterations);
-  void setLargeSigmaSq(float depth_range);
-private:
-  DeviceImage<float> u_;
-  DeviceImage<float> u_head_;
-  DeviceImage<float2> p_;
-  DeviceImage<float> g_;
 
-  denoise::DeviceData * host_ptr;
-  denoise::DeviceData * dev_ptr;
+struct DeviceData
+{
+  DeviceData(
+      DeviceImage<float>  * const u_dev_ptr,
+      DeviceImage<float>  * const u_head_dev_ptr,
+      DeviceImage<float2> * const p_dev_ptr,
+      DeviceImage<float>  * const g_dev_ptr,
+      const size_t &w,
+      const size_t &h);
+  const float L;
+  const float tau;
+  const float sigma;
+  const float theta;
 
-  dim3 dim_block_;
-  dim3 dim_grid_;
+  DeviceImage<float>  * const u;
+  DeviceImage<float>  * const u_head;
+  DeviceImage<float2> * const p;
+  DeviceImage<float>  * const g;
+
+  const size_t width;
+  const size_t height;
+
+  float large_sigma_sq;
+  float lambda;
 };
+
+} // denoise namespace
 
 } // rmd namespace
 
-#endif // RMD_DEPTHMAP_DENOISER_CUH
+#endif // RMD_DENOISE_DEVICE_DATA_CUH
