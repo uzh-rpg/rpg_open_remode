27e2eb17659a1a795d7d4476a4ea92f38ae496fb
pizzoli <matia.pizzoli@gmail.com>
2015-10-25 17:46:26 +0100

    Added skeleton for the depthmap ROS node

---
+++ src/depthmap_node.cpp
@@ -0,0 +1,24 @@
+// This file is part of REMODE - REgularized MOnocular Depth Estimation.
+//
+// Copyright (C) 2014 Matia Pizzoli <matia dot pizzoli at gmail dot com>
+// Robotics and Perception Group, University of Zurich, Switzerland
+// http://rpg.ifi.uzh.ch
+//
+// REMODE is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or any later version.
+//
+// REMODE is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <rmd/depthmap_node.h>
+
+void rmd::DepthmapNode::denseInputCallback(
+    const svo_msgs::DenseInputConstPtr &dense_input)
+{
+  std::cout << "svo msg received" << std::endl;
+}
